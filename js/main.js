8c6a902e5dbfe56d38d99a099d86829e097ecaab
Nicolas <nicolas_thiphaine@yahoo.fr>
2022-06-06 22:07:27 +0900

    css cleanup

--- js/main.js
+++ js/main.js
@@ -15,3 +15,36 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+/* Mobile Menu  */ 
+$WIN = $(window);
+var MobileMenu = function() {
+
+  var toggleButton = $('.header-menu-toggle'),
+      nav = $('.header-nav-wrap');
+
+  toggleButton.on('click', function(event){
+      event.preventDefault();
+
+      toggleButton.toggleClass('is-clicked');
+      nav.slideToggle();
+  });
+
+  if (toggleButton.is(':visible')) nav.addClass('mobile');
+
+  $WIN.on('resize', function() {
+      if (toggleButton.is(':visible')) nav.addClass('mobile');
+      else nav.removeClass('mobile');
+  });
+
+  nav.find('a').on("click", function() {
+
+      if (nav.hasClass('mobile')) {
+          toggleButton.toggleClass('is-clicked');
+          nav.slideToggle(); 
+      }
+  });
+
+};
+
+MobileMenu();
