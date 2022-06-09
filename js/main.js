4b5c8cc1b449af358a40e4a2b9f364495911386b
Nicolas <nicolas_thiphaine@yahoo.fr>
2022-06-09 09:26:16 +0900

    works update

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ var header = document.getElementById("id-header");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+/* 
+Function to make the header sticky on scroll 
+*/
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
@@ -16,13 +20,16 @@ function myFunction() {
   }
 }
 
-// Mobile Menu
+/* 
+Function to swap the header to the mobile menu
+*/
 $WIN = $(window);
 var MobileMenu = function() {
 
-  var toggleButton = $('.header-menu-toggle'),
-      nav = $('.header-nav-wrap');
+  // Get the mobile menu button and the header
+  var toggleButton = $('.header-menu-toggle'), nav = $('.header-nav-wrap');
 
+  // We create a event that listens to the click
   toggleButton.on('click', function(event){
       event.preventDefault();
 
@@ -30,6 +37,7 @@ var MobileMenu = function() {
       nav.slideToggle();
   });
 
+  // We check if the conditions are met to go in mobile display
   if (toggleButton.is(':visible')) nav.addClass('mobile');
 
   $WIN.on('resize', function() {
@@ -47,16 +55,22 @@ var MobileMenu = function() {
 
 };
 
-// Highlight current page in the header
+/*
+Highlight current page in the header
+*/
+
 const scrollHandler = () => {
 
+  // Get the header
   let menu = document.querySelector('.header-main-nav')
 
+  // Get the sections
   let intro = document.getElementById('intro');
   let about = document.getElementById('about');
   let skills = document.getElementById('skills');
   let works = document.getElementById('works');
 
+  // Get the header and sections position
   let pos_menu = window.pageYOffset + menu.offsetHeight;
 
   let pos_intro = intro.offsetTop + intro.offsetHeight;
@@ -64,18 +78,22 @@ const scrollHandler = () => {
   let pos_skills = skills.offsetTop + skills.offsetHeight;
   let pos_works = works.offsetTop + works.offsetHeight;
   
+  // Get the distances between the header and the sections
   let distance_intro = pos_intro - pos_menu;
   let distance_about = pos_about - pos_menu;
   let distance_skills = pos_skills - pos_menu;
   let distance_works = pos_works - pos_menu;
 
+  // Determine the current section
   let min = Math.min(...[distance_intro, distance_about, distance_skills, distance_works].filter(num => num > 0));
 
+  // Remove the current class from all sections in the header (re-initialisation)
   document.querySelectorAll('.header-main-nav .Item')[0].classList.remove('current');
   document.querySelectorAll('.header-main-nav .Item')[1].classList.remove('current');
   document.querySelectorAll('.header-main-nav .Item')[2].classList.remove('current');
   document.querySelectorAll('.header-main-nav .Item')[3].classList.remove('current');
 
+  // Add the current class to the current section
   if(min === distance_intro) document.querySelectorAll('.header-main-nav .Item')[0].classList.add('current');
   if(min === distance_about) document.querySelectorAll('.header-main-nav .Item')[1].classList.add('current');
   if(min === distance_skills) document.querySelectorAll('.header-main-nav .Item')[2].classList.add('current');
